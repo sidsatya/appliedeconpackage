cecbce0d26394a31fecb2e404e3d07fa95e02182
sidsatya <sidsatya@berkeley.edu>
2025-03-16 21:44:19 -0400

    added code

--- R/myfunc.R
+++ R/myfunc.R
@@ -12,3 +12,29 @@ myfunc <- function(a, b) {
   result <- a * b + a
   return(result)
 }
+
+#' OLS estimator
+#'
+#' @param y A numerical vector, outcomes. 
+#' @param X Also a numerical vector, observations.
+#'
+#' @return A numerical vector of (X^T X)^(-1) X^T y.
+#' @export
+estimate_beta <- function(y, X) {
+  beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
+  return(beta_hat)
+}
+
+#' A custom ggplot2 theme with no grid lines, a light gray background, and bold axis titles.
+#'
+#' @return A ggplot2 theme object.
+#' @export
+my_theme <- function() {
+  ggplot2::theme_minimal() +  
+    ggplot2::theme(
+      panel.grid.major = ggplot2::element_blank(),  
+      panel.grid.minor = ggplot2::element_blank(),  
+      plot.background = ggplot2::element_rect(fill = "lightgray", color = NA),
+      axis.title = ggplot2::element_text(size = 14, face = "bold")
+    )
+}
